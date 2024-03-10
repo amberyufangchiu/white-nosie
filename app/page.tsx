2b2c80c27a6746808ac60ce64dd823d1e3ed9eda
Amber Chiu <suzumiya1235@gmail.com>
2024-03-10 23:39:05 +0800

    feat: :lipstick: adjust background color and box

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Card, CardContent, CardFooter } from "@/components/Card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Volume } from "@/components/Volume";
-import { ScrollArea } from "@/components/ScrollArea";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Sounds } from "@/components/Sounds";
+import { Button } from "@/components/ui/button";
 
 type UseAudioReturnType = [boolean, () => void];
 
@@ -68,14 +69,14 @@ const AudioPlayer = ({ src, isSelected }: AudioPlayerProps) => {
     <Card
       key={crypto.randomUUID()}
       onClick={toggle}
-      className={playing ? "bg-red-200" : undefined}
+      className={playing ? "bg-gray-200" : undefined}
     >
       <CardContent className="flex items-center justify-center">
         {src?.icon}
       </CardContent>
       <CardFooter>
         <Volume
-          className={playing ? "bg-red-200" : undefined}
+          className={playing ? "bg-gray-200" : undefined}
           value={volume}
           onChange={(event) => {
             setVolume((event.target as HTMLInputElement).valueAsNumber);
@@ -94,14 +95,23 @@ export default function Home() {
   const forest = ["Birds", "Wind", "SoftRain", "River"];
 
   return (
+    <main className="flex items-center flex-col justify-center w-screen h-screen">
       <div className="flex gap-2">
         <Button onClick={() => setMode([])}>Stop</Button>
         <Button onClick={() => setMode(relax)}>Relax</Button>
         <Button onClick={() => setMode(focus)}>Focus</Button>
         <Button onClick={() => setMode(forest)}>Forest</Button>
       </div>
+      <ScrollArea className="h-96 w-3/5 p-4">
+        <div className="flex flex-wrap gap-4 justify-center items-center">
           {sounds.map((src) => {
+            return (
+              <AudioPlayer
+                key={crypto.randomUUID()}
+                src={src}
                 isSelected={mode.includes(src.name || "")}
+              />
+            );
           })}
         </div>
       </ScrollArea>
